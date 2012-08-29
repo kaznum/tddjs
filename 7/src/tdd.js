4f4d5420fe94ea56f6cc4affa2f21c0094330b59
Kazuya NUMATA <numata@gmail.com>
2012-08-29 09:44:30 +0900

    Add tdd.js which contains tddjs.isOwnProperty

---
+++ 7/src/tdd.js
@@ -0,0 +1,70 @@
+/*jslint indent: 2, onevar: false, plusplus: false, eqeqeq: false, nomen: false*/
+var tddjs = (function () {
+  function namespace(string) {
+    var object = this;
+    var levels = string.split(".");
+
+    for (var i = 0, l = levels.length; i < l; i++) {
+      if (typeof object[levels[i]] == "undefined") {
+        object[levels[i]] = {};
+      }
+
+      object = object[levels[i]];
+    }
+
+    return object;
+  }
+
+  return {
+    namespace: namespace
+  };
+}());
+
+(function () {
+  var id = 0;
+
+  function uid(object) {
+    if (typeof object.__uid != "number") {
+      object.__uid = id++;
+    }
+
+    return object.__uid;
+  }
+
+  if (typeof tddjs == "object") {
+    tddjs.uid = uid;
+  }
+}());
+
+(function () {
+  function iterator(collection) {
+    var index = 0;
+    var length = collection.length;
+
+    function next() {
+      var item = collection[index++];
+      next.hasNext = index < length;
+
+      return item;
+    }
+
+    next.hasNext = index < length;
+
+    return next;
+  }
+
+  if (typeof tddjs == "object") {
+    tddjs.iterator = iterator;
+  }
+}());
+
+tddjs.isOwnProperty = (function () {
+    var hasOwn = Object.prototype.hasOwnProperty;
+    if (typeof hasOwn == "function") {
+	return function (object, property) {
+	    return hasOwn.call(object, property);
+	};
+    } else {
+	// Provide an emulation if you can live with possibly inaccurate results
+    }
+}());
