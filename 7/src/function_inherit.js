942fa40747dbb28efc08579fc240d2f30d4a3863
Kazuya NUMATA <numata@gmail.com>
2012-08-29 19:28:51 +0900

    implement Function.prototype.inherit

---
+++ 7/src/function_inherit.js
@@ -0,0 +1,11 @@
+if (!Function.prototype.inherit) {
+  (function () {
+    function F() {}
+
+    Function.prototype.inherit = function (superFn) {
+      F.prototype = superFn.prototype;
+      this.prototype = new F();
+      this.prototype.constructor = this;
+    };
+  }());
+}
