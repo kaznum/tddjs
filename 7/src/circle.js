cabb1eebd92932a90c7fbd13357c91a1e4a66175
Kazuya NUMATA <numata@gmail.com>
2012-08-29 19:06:45 +0900

    Change indent to 2 spaces.

--- 7/src/circle.js
+++ 7/src/circle.js
@@ -1,32 +1,32 @@
 function Circle(radius) {
-    if (!(this instanceof Circle)) {
-	throw new Error();
-    }
-    this.radius = radius;
+  if (!(this instanceof Circle)) {
+    throw new Error();
+  }
+  this.radius = radius;
 }
 
 (function (p) {
-    p.diameter = function () {
-	return this.radius * 2;
-    };
+  p.diameter = function () {
+    return this.radius * 2;
+  };
 
-    p.circumference = function () {
-	return this.diameter() * Math.PI;
-    };
+  p.circumference = function () {
+    return this.diameter() * Math.PI;
+  };
 
-    p.area = function () {
-	return this.radius * this.radius * Math.PI;
-    };
+  p.area = function () {
+    return this.radius * this.radius * Math.PI;
+  };
 }(Circle.prototype));
 
 function Sphere(radius) {
-    this.radius = radius;
+  this.radius = radius;
 }
 Sphere.prototype = (function () {
-    function F() {};
-    F.prototype = Circle.prototype;
+  function F() {};
+  F.prototype = Circle.prototype;
 
-    return new F();
+  return new F();
 }());
 //(基本)  new で生成されたオブジェクトの[[Prototype]]には、コンストラクタのprototypeへの参照が設定される
 // new Sphere(6)で生成されたオブジェクトの[[Prototype]] == Sphere.prototype(Fのオブジェクト)
@@ -34,14 +34,14 @@ Sphere.prototype = (function () {
 // Fのオブジェクト.[[Prototype]] == Circle.prototypeへの参照が設定される
 // Circleのprototype.[[Prototype]] == Object.prototype
 (function(p) {
-    p.constructor = Sphere;
+  p.constructor = Sphere;
 
-    p.area = function () {
-	return 4 * Circle.prototype.area.call(this);
-    }
+  p.area = function () {
+    return 4 * Circle.prototype.area.call(this);
+  }
 
-    p.volume = function () {
-	return 4/3 * Math.PI * this.radius * this.radius * this.radius;
-    }
+  p.volume = function () {
+    return 4/3 * Math.PI * this.radius * this.radius * this.radius;
+  }
 }(Sphere.prototype));
 
--- 7/test/circle_test.js
+++ 7/test/circle_test.js
@@ -1,42 +1,42 @@
 TestCase("CircleTest", {
-    "test inspect objects" : function () {
-	var circ = new Circle(6);
-	var circ2 = { radius: 6 };
+  "test inspect objects" : function () {
+    var circ = new Circle(6);
+    var circ2 = { radius: 6 };
 
-	assertTrue(circ instanceof Object);
-	assertTrue(circ instanceof Circle);
-	assertTrue(circ2 instanceof Object);
-	assertEquals(Circle, circ.constructor);
-	assertEquals(Object, circ2.constructor);
-    },
-    "test should create another object of same kind": function () {
-	var circle = new Circle(6);
-	var circle2 = new circle.constructor(9);
-	assertEquals(circle.constructor, circle2.constructor);
-	assertTrue(circle2 instanceof Circle);
-    },
-    "test should inherit properties from Circle.prototype" : function () {
-	var circle = new Circle(6);
-	assertEquals(12, circle.diameter());
-    },
-    "test constructor is Object when prototype is overridden": function () {
-	function Circle() {}
-	Circle.prototype = {};
-	assertEquals(Object, new Circle().constructor);
-    },
-    "test calling prototype without 'new' returns undefined": function () {
-	assertException(function() { Circle(6);}, "Error")
-    },
+    assertTrue(circ instanceof Object);
+    assertTrue(circ instanceof Circle);
+    assertTrue(circ2 instanceof Object);
+    assertEquals(Circle, circ.constructor);
+    assertEquals(Object, circ2.constructor);
+  },
+  "test should create another object of same kind": function () {
+    var circle = new Circle(6);
+    var circle2 = new circle.constructor(9);
+    assertEquals(circle.constructor, circle2.constructor);
+    assertTrue(circle2 instanceof Circle);
+  },
+  "test should inherit properties from Circle.prototype" : function () {
+    var circle = new Circle(6);
+    assertEquals(12, circle.diameter());
+  },
+  "test constructor is Object when prototype is overridden": function () {
+    function Circle() {}
+    Circle.prototype = {};
+    assertEquals(Object, new Circle().constructor);
+  },
+  "test calling prototype without 'new' returns undefined": function () {
+    assertException(function() { Circle(6);}, "Error")
+  },
 
-    "test spheres are circles in 3D": function () {
-	var radius = 3;
-	var sphere = new Sphere(radius);
+  "test spheres are circles in 3D": function () {
+    var radius = 3;
+    var sphere = new Sphere(radius);
 
-	assertTrue(sphere instanceof Sphere);
-	assertTrue(sphere instanceof Circle);
-	assertTrue(sphere instanceof Object);
-	assertEquals(6, sphere.diameter());
-	assertEquals(36 * Math.PI,sphere.area());
-	assertEquals(4 / 3 * 27 * Math.PI,sphere.volume());
-    }
+    assertTrue(sphere instanceof Sphere);
+    assertTrue(sphere instanceof Circle);
+    assertTrue(sphere instanceof Object);
+    assertEquals(6, sphere.diameter());
+    assertEquals(36 * Math.PI,sphere.area());
+    assertEquals(4 / 3 * 27 * Math.PI,sphere.volume());
+  }
 });
