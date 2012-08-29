17caed5ac43732c049d2f18c559a0ab0835a9694
Kazuya NUMATA <numata@gmail.com>
2012-08-29 20:52:31 +0900

    implement inherit2 for _super() method.

--- 7/src/function_inherit.js
+++ 7/src/function_inherit.js
@@ -10,3 +10,33 @@ if (!Function.prototype.inherit) {
     };
   }());
 }
+
+if (!Function.prototype.inherit2) {
+  (function () {
+    function F() {}
+
+    Function.prototype.inherit2 = function (superFn, methods) {
+      F.prototype = superFn.prototype;
+      this.prototype = new F();
+      this.prototype.constructor = this;
+
+      var subProto = this.prototype;
+
+      tddjs.each(methods, function (name, method) {
+	subProto[name] = function () {
+	  var returnValue;
+	  var oldSuper = this._super;
+	  this._super = superFn.prototype[name];
+
+	  try {
+	    returnValue = method.apply(this, arguments);
+	  } finally {
+	    this._super = oldSuper;
+	  }
+
+	  return returnValue;
+	};
+      });
+    };
+  }());
+}
--- 7/test/function_inherit_test.js
+++ 7/test/function_inherit_test.js
@@ -13,5 +13,38 @@ TestCase("FunctionInheritTest", {
     SubFn.inherit(SuperFn);
 
     assertEquals(SuperFn.prototype, SubFn.prototype._super);
+  },
+
+  "test super should call method of same name on prototype" : function () {
+    function Person(name) {
+      this.name = name;
+    }
+    Person.prototype = {
+      constructor: Person,
+      getName: function () {
+	return this.name;
+      },
+      speak: function () {
+	return "Hello";
+      }
+    };
+
+    function LoudPerson(name) {
+      Person.call(this, name);
+    }
+
+    LoudPerson.inherit2(Person, {
+      getName: function () {
+	return this._super().toUpperCase();
+      },
+      speak: function () {
+	return this._super() + "!!!";
+      }
+    });
+
+    var np = new LoudPerson("Chris");
+
+    assertEquals("CHRIS", np.getName());
+    assertEquals("Hello!!!", np.speak());
   }
 });
