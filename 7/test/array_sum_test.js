7e95ac0ca9e224d866742915c49f132d9044aa6d
Kazuya NUMATA <numata@gmail.com>
2012-08-29 09:36:06 +0900

    add check with hasOwnProperty

--- 7/test/array_sum_test.js
+++ 7/test/array_sum_test.js
@@ -2,5 +2,20 @@ TestCase("ArraySumTest", {
     "test should summarize numbers in array": function () {
 	var array = [1,2,3,4,5,6];
 	assertEquals(21, array.sum());
+    },
+    "test looping should only iterate over own properties" : function () {
+	var person = {
+	    name: "Kazuya",
+	    profession: "Programmer",
+	    location: "Sapporo"
+	};
+	var result = [];
+	for ( var prop in person ) {
+	    if (person.hasOwnProperty(prop)) {
+		result.push(prop);
+	    }
+	}
+	var expected = ["location", "name", "profession"];
+	assertEquals(expected, result.sort());
     }
 });
