b94faf7174e2f19423779c95265567ece5c9cac8
Kazuya NUMATA <numata@gmail.com>
2012-09-01 10:53:10 +0900

    add test for explicit method-borrowing

--- 7/test/array_prototype_test.js
+++ 7/test/array_prototype_test.js
@@ -8,6 +8,16 @@ TestCase("ArrayPrototypeTest", {
 
     var result = addToArray([],1,2,3);
     assertEquals([1,2,3], result);
-    
+  },
+  "test arguments should borrow explicitly from Array.prototype": function () {
+    function addToArray() {
+      arguments.slice = Array.prototype.slice;
+      var args = arguments.slice();
+      var arr = args.shift();
+      return arr.concat(args);
+    }
+
+    var result = addToArray([],1,2,3);
+    assertEquals([1,2,3], result);
   }
 });
