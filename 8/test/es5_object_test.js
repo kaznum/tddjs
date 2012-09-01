fa54d6a7e094a8ef37639d304ef2cfc56492ab9c
Kazuya NUMATA <numata@gmail.com>
2012-09-02 08:24:54 +0900

    add tests for several ways of inheritance

--- 8/test/es5_object_test.js
+++ 8/test/es5_object_test.js
@@ -19,5 +19,39 @@ TestCase("ES5ObjectTest", {
     delete circle.radius;
 
     assertEquals(3, circle.radius);
+  },
+  "test es3 inheritance via constructors": function () {
+    var circle = {};
+    function CircleProxy() {}
+    CircleProxy.prototype = circle;
+    var sphere = new CircleProxy();
+
+    assert(circle.isPrototypeOf(sphere));
+  },
+  "test inheritance via proprietary __proto__": function () {
+    var circle = {};
+    var sphere = {};
+    sphere.__proto__ = circle;
+    assert(circle.isPrototypeOf(sphere));
+  },
+  "test inheritance via es5 style": function () {
+    var circle = {};
+    var sphere = Object.create(circle);
+    assert(circle.isPrototypeOf(sphere));
+    assertEquals(circle, Object.getPrototypeOf(sphere));
+  },
+  "test Object.create with properties": function () {
+    var circle = {};
+    var sphere = Object.create(circle, {
+      radius: {
+	value: 3,
+	writable: false,
+	configurable: false,
+	enumerable: true
+      }
+    });
+    assertEquals(3, sphere.radius);
+    assert(circle.isPrototypeOf(sphere));
+    assertEquals(circle, Object.getPrototypeOf(sphere));
   }
 });
