0eb0a0ba912f4a739255063fc425d708ea712f06
Kazuya NUMATA <numata@gmail.com>
2012-09-06 14:43:17 +0900

    Implement HTML and css and make use of tabController

--- 9/src/tab_controller.js
+++ 9/src/tab_controller.js
@@ -1,6 +1,7 @@
 (function () {
   var dom = tddjs.dom;
 
+  // create することで、画面内に複数のタブ群が存在しても管理できる。
   function create(element) {
     if (!element || typeof element.className != "string") {
       throw new TypeError("element is not an element");
@@ -51,4 +52,43 @@
     onTabChange: function(curr, prev) {},
     tabTagName: "a"
   };
+}());
+
+
+(function() {
+  function getPanel(element) {
+    if (!element|| typeof element.href != "string") {
+      return null;
+    }
+
+    // URLを#形式にして画面遷移を行わない場合、ブラウザのアドレス部が変更されるが、
+    // 画面遷移は行われない。（ブラウザヒストリが残り戻るボタンが有効になるが、
+    // 戻るボタンを押しても、画面の切り替えは行われない
+    var target = element.href.replace(/.*#/, "");
+    var panel = document.getElementsByName(target)[0];
+    while (panel && panel.tagName.toLowerCase() != "div") {
+      panel = panel.parentNode;
+    }
+
+    return panel;
+  }
+
+  if (typeof document == "undefined" || !document.getElementById) {
+    return;
+  }
+
+  var dom = tddjs.dom;
+  var ol = document.getElementById("news-tabs");
+
+  try {
+    var controller = tddjs.ui.tabController.create(ol);
+    dom.addClassName(ol.parentNode, "js-tabs");
+
+    controller.onTabChange = function (curr, prev) {
+      dom.removeClassName(getPanel(prev), "active-panel");
+      dom.addClassName(getPanel(curr), "active-panel");
+    };
+
+    controller.activateTab(ol.getElementsByTagName("a")[0]);
+  } catch (e) {}
 }());
