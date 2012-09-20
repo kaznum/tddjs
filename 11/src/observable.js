5c65316f27361dcff9061f812acfad57f62992b6
Kazuya NUMATA <numata@gmail.com>
2012-09-20 12:29:28 +0900

    implement Observable#hasObserver

--- 11/src/observable.js
+++ 11/src/observable.js
@@ -3,12 +3,21 @@ tddjs.namespace("util");
   function Observable() {
     this.observers = [];
   }
-
   tddjs.util.Observable = Observable;
 
   function addObserver(observer) {
     this.observers.push(observer);
   }
-
   Observable.prototype.addObserver = addObserver;
+
+  function hasObserver(observer) {
+    for (var i = 0, l = this.observers.length; i < l; i++) {
+      if (this.observers[i] == observer) {
+        return true;
+      }
+    }
+    return false;
+  }
+  Observable.prototype.hasObserver = hasObserver;
+
 }());
--- 11/test/observable_test.js
+++ 11/test/observable_test.js
@@ -9,3 +9,19 @@ TestCase("ObservableAddObserverTest", {
     assertEquals(observers, observable.observers);
   }
 });
+
+TestCase("ObservableHasObserverTest", {
+  "test should return true when has observer": function () {
+    var observable = new tddjs.util.Observable();
+    var observer = function () {};
+
+    observable.addObserver(observer);
+    assertTrue(observable.hasObserver(observer));
+  },
+
+  "test should return false when no observers" : function () {
+    var observable = new tddjs.util.Observable();
+
+    assertFalse(observable.hasObserver(function () {}));
+  }
+});
