25d92d77bfa9ad9cff47306784e3f1a467c92a93
Kazuya NUMATA <numata@gmail.com>
2012-09-23 01:35:47 +0900

    implement ajax.create

---
+++ 12/src/ajax.js
@@ -0,0 +1,21 @@
+(function() {
+  var xhr;
+  var ajax = tddjs.namespace("ajax");
+  var options = [
+    function () {
+      return new ActiveXObject("Microsoft.XMLHTTP");
+    },
+
+    function () {
+      return new XMLHttpRequest();
+    }
+  ];
+
+  for (var i = 0, l = options.length; i < l; i++) {
+    try {
+      xhr = options[i]();
+      ajax.create = options[i];
+      break;
+    } catch (e) {}
+  }
+}());
