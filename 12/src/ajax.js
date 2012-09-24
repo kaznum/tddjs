1ec2d43c99fd12b4da54350caa4f58c2c763b501
Kazuya NUMATA <numata@gmail.com>
2012-09-25 01:10:16 +0900

    Move some code for Ajax request to request.js

--- 12/src/ajax.js
+++ 12/src/ajax.js
@@ -25,38 +25,3 @@
     } catch (e) {}
   }
 }());
-
-(function () {
-  var ajax = tddjs.namespace("ajax");
-
-  function requestComplete(transport, options) {
-    if (transport.status == 200) {
-      if (typeof options.success == "function") {
-        options.success(transport);
-      }
-    }
-  }
-
-  if (!ajax.create) {
-    return;
-  }
-
-  function get(url, options) {
-    if (typeof url != "string") {
-      throw new TypeError("URL should be string");
-    }
-
-    options = options || {};
-    var transport = tddjs.ajax.create();
-
-    transport.open("GET", url, true);
-    transport.onreadystatechange = function () {
-      if (transport.readyState == 4) {
-        requestComplete(transport, options);
-      }
-    };
-    transport.send();
-  };
-
-  ajax.get = get;
-}());
