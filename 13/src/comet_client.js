081486d6ed9f74841babae2c16951002368a881e
Kazuya Numata <numata@gmail.com>
2013-01-05 15:12:04 +0900

    Make dispatch call observers.notify (Listing 13.48)

--- 13/src/comet_client.js
+++ 13/src/comet_client.js
@@ -2,7 +2,13 @@
   var ajax = tddjs.namespace("ajax");
   ajax.cometClient = {};
 
-  function dispatch() {
+  function dispatch(data) {
+    var observers = this.observers;
+    tddjs.each(data, function(topic, events) {
+      for (var i = 0, l = events.length; i < l; i++) {
+        observers.notify(topic, events[i]);
+      }
+    });
   }
 
   ajax.cometClient = {
--- 13/test/comet_client_test.js
+++ 13/test/comet_client_test.js
@@ -10,6 +10,18 @@
       var client = Object.create(ajax.cometClient);
 
       assertFunction(client.dispatch);
+    },
+
+    "test dispatch should notify observers": function () {
+      var client = Object.create(ajax.cometClient);
+      client.observers = { notify: stubFn() };
+      client.dispatch({ someEvent: [{id : 1234}]});
+
+      var args = client.observers.notify.args;
+
+      assert(client.observers.notify.called);
+      assertEquals("someEvent", args[0]);
+      assertEquals({id: 1234}, args[1]);
     }
   });
 }());
