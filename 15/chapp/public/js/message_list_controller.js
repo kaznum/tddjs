a42bb6a37ead5d34d92bdc47e67d6662f8a95db8
Kazuya NUMATA <numata@gmail.com>
2013-09-17 11:51:57 +0900

    Add message_list_controller.js to web server

---
+++ 15/chapp/public/js/message_list_controller.js
@@ -0,0 +1,45 @@
+(function () {
+
+  if (typeof tddjs == "undefined" ||
+      typeof document == "undefined" ||
+      !document.createElement) {
+    return;
+    }
+
+  var element = document.createElement("dl");
+
+  if (!element.appendChild ||
+      typeof element.innerHTML != "string") {
+    return;
+  }
+  element = null;
+  
+  var chat = tddjs.namespace("chat");
+  function setModel(model) {
+    model.observe("message", this.addMessage.bind(this));
+  }
+
+  function addMessage(message) {
+    if (this.prevUser != message.user) {
+      var user = document.createElement("dt");
+      user.innerHTML = "@" + message.user;
+      this.view.appendChild(user);
+      this.prevUser = message.user;
+    }
+
+    var msg = document.createElement("dd");
+    msg.innerHTML = message.message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    this.view.appendChild(msg);
+  }
+
+  function setView(element) {
+    element.className = "js-chat";
+    this.view = element;
+  }
+
+  chat.messageListController = {
+    setModel: setModel,
+    setView: setView,
+    addMessage: addMessage
+  };
+}());
